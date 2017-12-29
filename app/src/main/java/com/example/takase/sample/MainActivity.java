86a9dfbc905a064338a027557e205c7cc1f9f6a7
takase <takase@e-2.co.jp>
2017-12-30 03:01:08 +0900

    サイトをwebviewで表示

--- app/src/main/java/com/example/takase/sample/MainActivity.java
+++ app/src/main/java/com/example/takase/sample/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.takase.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.HttpAuthHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView)findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host, String realm) {
+                handler.proceed("lumielinabrand", "e2devel");
+            }
+        });
+
+        try {
+            if (savedInstanceState == null) {
+                mWebView.loadUrl(getString(R.string.site_url));
+
+                if (!mWebView.getUrl().equals(getString(R.string.site_url))) {
+                    throw new Exception();
+                }
+
+            } else {
+                mWebView.restoreState(savedInstanceState);
+            }
+        } catch (Exception e) {
+            mWebView.loadUrl(getString(R.string.site_url));
+            Toast.makeText(this, "不正なページが指定されました。", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        mWebView.saveState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        mWebView.goBack();
     }
 }
